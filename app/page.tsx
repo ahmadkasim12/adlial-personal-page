abd438da4e16f949a3eb1f18033d965101b5c39b
ahmadkasim12 <106042415+ahmadkasim12@users.noreply.github.com>
2025-09-02 08:59:52 +0700

    personal page update 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,125 @@
-import Image from "next/image";
+import { Icon } from "@iconify/react";
+import Image from 'next/image';
+
+const data = [
+  {
+    id: 1,
+    name: "ProjectSynergy",
+    alias: "Machinery Maintenance and Item Request Platform",
+    description: "A 12th Grade final assignment developed specifically for Pabrik Gula Rendeng",
+    link: "https://purrfectbites.rplrus.com/",
+  },
+  {
+    id: 2,
+    name: "quiqiNote.g",
+    alias: "Personal Note-taking Application",
+    description: "Flutter-based note-taking application",
+    link: "https://purrfectbites.rplrus.com/",
+  },
+  {
+    id: 3,
+    name: "Music Player",
+    alias: "Audio Player",
+    description: "Flutter-based music player application with metadata fetching and customizations capabilities",
+    link: "https://purrfectbites.rplrus.com/",
+  },
+  {
+    id: 4,
+    name: "Purrfectbites",
+    alias: "PAS Web Dev",
+    description: "10th Grade Final Semester Project for Web Development Course",
+    link: "https://purrfectbites.rplrus.com/",
+  }
+];
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="font-sans grid grid-rows-[1fr_20px] gap-6 items-center justify-items-center min-h-screen p-12 sm:p-20">
+      <main className="max-w-[47em] max-h-[28em] overflow-y-scroll scrollbar-hidden gap-[32px] row-start-1 items-center sm:items-start">
+        
+        <div className="pointer-events-none absolute left-0 right-0 h-8 bg-gradient-to-b from-[#0a0a0a] to-transparent"></div>
+        
+        <div className="grid lg:grid-rows-[120px_1fr_120px]">
+          <div className="row-start-2 grid grid-col items-center gap-6 sm:items-start">
+          <div className="grid gap-6 items-center justify-items-center sm:grid-cols-[auto_1fr] sm:justify-items-start">
+            {/* <img src={"https://i.pravatar.cc/300"} className="w-[80px] h-[80px] rounded-sm border border-white/50"></img> */}
+            <Image src="/avatar.png" alt="Picture of the author" width={80} height={80} className="w-[80px] h-[80px] rounded-sm border border-white/50 object-cover"/>
+            <div className="flex flex-col w-full">
+              <h1 className="text-lg font-bold text-center sm:text-left">
+                Adlialfakhri Ziyadatulah
+              </h1>
+              <p className="text-md text-center sm:text-left text-white/60">
+                An enthusiastic and passionate individual who is eager to learn and explore new things.
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-[302px_1fr] gap-6">
+            <div className="flex flex-col">
+              <span className="text-2xl font-bold">Projects</span>
+              <span className="text-md text-white/60">This section resides for all projects that I've done throughout my journey.</span>
+            </div>
+            <div className="grid grid-cols-1 items-center lg:items-start rounded-sm border border-white/20">
+              {data.map(item => (
+                <div key={item.id} className="grid grid-rows-2 gap-1 px-8 py-6 text-white/60 border-b border-white/20 transition-colors">
+                  <a
+                    href={item.link}
+                    className="flex justify-self-stretch items-center min-w-0"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="font-bold shrink-0 hover:underline">{item.name}</span>
+                    <span className="px-2">–</span>
+                    <span className="relative block flex-1 min-w-0 overflow-hidden before:content-[''] before:absolute before:inset-y-0 before:right-0 before:w-6 before:bg-gradient-to-l before:from-[#0a0a0a] before:to-transparent before:pointer-events-none">
+                      <span className="inline-block whitespace-nowrap hover:animate-marquee">
+                        {item.alias}
+                      </span>
+                    </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                    >
+                      <g fill="currentColor">
+                        <path d="M15.64 7.025h-3.622v-2h7v7h-2v-3.55l-4.914 4.914l-1.414-1.414z" />
+                        <path d="M10.982 6.975h-6v12h12v-6h-2v4h-8v-8h4z" />
+                      </g>
+                    </svg>
+                  </a>
+                  <span className="text-sm bg-white/10 rounded-sm px-1 justify-self-start">
+                    {item.description}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+        </div>
+
+        <div className="pointer-events-none absolute bottom-47 left-0 right-0 h-8 bg-gradient-to-t from-[#0a0a0a] to-transparent"></div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+      <footer className="row-start-2 flex flex flex-col items-center justify-center">
+        <div className="flex gap-4 items-center">
+          <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/adlialfakhri-ziyadatullah/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <Icon icon="ri:linkedin-fill" />
+          LinkedIn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/ahmadkasim12/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          <Icon icon="iconoir:github" />
+          GitHub
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        </div>
+        <span className="text-center text-white/30">Website are still in development so there will be more to come! (≧∀≦)ゞ</span>
       </footer>
     </div>
   );
